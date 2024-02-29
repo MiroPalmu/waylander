5341c4645652e2a953fc79dd5855c1b45c7dc04f
Miro Palmu <email@miropalmu.cc>
2024-02-29 16:33:56 +0200

    feat(bit): add byte_buffer

---
+++ include/bit_fiddling.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <array>
+#include <bit>
+#include <concepts>
+#include <cstddef>
+#include <cstring>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+namespace ger {
+namespace bit {
+
+template<std::size_t N, typename... I>
+concept total_memory_usage_is = (N == (0 + ... + sizeof(I)));
+
+/// Stores bytes in std::array with native endianness
+template<std::size_t N>
+class byte_buff {
+    std::array<std::byte, N> data_{};
+
+  public:
+    template<std::unsigned_integral... I>
+        requires total_memory_usage_is<N, I...>
+    [[nodiscard]] constexpr byte_buff(const I... message) {
+        auto pos = std::size_t{ 0 };
+
+        auto write_bytes_to_pos = [&]<std::size_t... Bytes>(const std::unsigned_integral auto x,
+                                                            std::index_sequence<Bytes...>) {
+            ((data_[pos + Bytes] =
+                  std::bit_cast<std::byte>(static_cast<std::byte>(x >> 8 * Bytes))),
+             ...);
+        };
+
+        [[maybe_unused]] auto write_and_increment_pos = [&](const std::unsigned_integral auto x) {
+            write_bytes_to_pos(x, std::make_index_sequence<sizeof(decltype(x))>{});
+
+            pos += sizeof(decltype(x));
+        };
+
+        (write_and_increment_pos(message), ...);
+    }
+
+    [[nodiscard]] constexpr auto bytes(this auto&& self) -> std::span<std::byte const, N> {
+        return self.data_;
+    }
+
+    [[nodiscard]] friend constexpr bool operator==(const byte_buff& lhs,
+                                                   const byte_buff& rhs) = default;
+};
+
+template<std::unsigned_integral... I>
+byte_buff(I...) -> byte_buff<(0 + ... + sizeof(I))>;
+
+} // namespace bit
+} // namespace ger
---
+++ tests/test_byte_buff.cpp
@@ -0,0 +1,180 @@
+#include <boost/ut.hpp> // import boost.ut;
+
+#include <cstddef>
+#include <ranges>
+
+#include "bit_fiddling.hpp"
+
+static_assert(ger::bit::total_memory_usage_is<0>);
+static_assert(ger::bit::total_memory_usage_is<1, std::uint8_t>);
+static_assert(ger::bit::total_memory_usage_is<4, std::uint32_t>);
+static_assert(ger::bit::total_memory_usage_is<8, std::uint64_t>);
+
+static_assert(ger::bit::total_memory_usage_is<0>);
+static_assert(ger::bit::total_memory_usage_is<2, std::uint8_t, std::uint8_t>);
+static_assert(ger::bit::total_memory_usage_is<5, std::uint8_t, std::uint32_t>);
+static_assert(ger::bit::total_memory_usage_is<9, std::uint8_t, std::uint64_t>);
+
+static_assert(ger::bit::total_memory_usage_is<3, std::uint8_t, std::uint8_t, std::uint8_t>);
+static_assert(ger::bit::total_memory_usage_is<6, std::uint8_t, std::uint32_t, std::uint8_t>);
+static_assert(ger::bit::total_memory_usage_is<10, std::uint8_t, std::uint64_t, std::uint8_t>);
+
+static_assert(not ger::bit::total_memory_usage_is<1>);
+static_assert(not ger::bit::total_memory_usage_is<2, std::uint8_t>);
+static_assert(not ger::bit::total_memory_usage_is<5, std::uint32_t>);
+static_assert(not ger::bit::total_memory_usage_is<9, std::uint64_t>);
+
+static_assert(not ger::bit::total_memory_usage_is<1>);
+static_assert(not ger::bit::total_memory_usage_is<3, std::uint8_t, std::uint8_t>);
+static_assert(not ger::bit::total_memory_usage_is<6, std::uint8_t, std::uint32_t>);
+static_assert(not ger::bit::total_memory_usage_is<10, std::uint8_t, std::uint64_t>);
+
+static_assert(not ger::bit::total_memory_usage_is<4, std::uint8_t, std::uint8_t, std::uint8_t>);
+static_assert(not ger::bit::total_memory_usage_is<7, std::uint8_t, std::uint32_t, std::uint8_t>);
+static_assert(not ger::bit::total_memory_usage_is<11, std::uint8_t, std::uint64_t, std::uint8_t>);
+
+int main() {
+    using namespace boost::ut;
+    using namespace ger::bit;
+    using namespace std;
+
+    "byte_buff is equality_comparable"_test = [] {
+        static_assert(equality_comparable<byte_buff<29>>);
+
+        constexpr auto buf_a1 = byte_buff<3>(uint8_t{ 0 }, uint8_t{ 1 }, uint8_t{ 2 });
+        constexpr auto buf_a2 = byte_buff<3>(uint8_t{ 0 }, uint8_t{ 1 }, uint8_t{ 2 });
+        constexpr auto buf_a3 = byte_buff<3>(uint8_t{ 0 }, uint8_t{ 2 }, uint8_t{ 2 });
+
+        expect(buf_a1 == buf_a2);
+        expect(buf_a1 != buf_a3);
+        expect(buf_a2 != buf_a3);
+    };
+
+    "byte_buff<0> is always equal to every other byte_buff<0>"_test = [] {
+        static_assert(equality_comparable<byte_buff<0>>);
+
+        constexpr auto buf_empty_a = byte_buff<0>{};
+        constexpr auto buf_empty_b = byte_buff<0>{};
+
+        expect(buf_empty_a == buf_empty_a);
+        expect(buf_empty_a == buf_empty_b);
+        expect(buf_empty_b == buf_empty_b);
+    };
+
+    "integers in integer message byte_buff constructor can alias each other"_test = [] {
+        constexpr auto one          = uint8_t{ 1 };
+        constexpr auto oneone       = static_cast<uint16_t>(one << 8 | one);
+        constexpr auto oneoneoneone = static_cast<uint32_t>(oneone << 16 | oneone);
+
+        constexpr auto buf_ones_a = byte_buff<4>(one, one, one, one);
+        constexpr auto buf_ones_b = byte_buff<4>(oneone, oneone);
+        constexpr auto buf_ones_c = byte_buff<4>(oneoneoneone);
+
+        constexpr auto buf_ones_d = byte_buff<4>(oneone, one, one);
+        constexpr auto buf_ones_e = byte_buff<4>(one, oneone, one);
+        constexpr auto buf_ones_f = byte_buff<4>(one, one, oneone);
+
+        // [a-c] together
+        expect(buf_ones_a == buf_ones_b);
+        expect(buf_ones_a == buf_ones_c);
+        expect(buf_ones_b == buf_ones_c);
+
+        // [d-f] together
+        expect(buf_ones_d == buf_ones_e);
+        expect(buf_ones_d == buf_ones_f);
+        expect(buf_ones_e == buf_ones_f);
+
+        // a with [d-f]
+        expect(buf_ones_a == buf_ones_d);
+        expect(buf_ones_a == buf_ones_e);
+        expect(buf_ones_a == buf_ones_f);
+
+        // b with [d-f]
+        expect(buf_ones_b == buf_ones_d);
+        expect(buf_ones_b == buf_ones_e);
+        expect(buf_ones_b == buf_ones_f);
+
+        // c with [d-f]
+        expect(buf_ones_c == buf_ones_d);
+        expect(buf_ones_c == buf_ones_e);
+        expect(buf_ones_c == buf_ones_f);
+    };
+
+    "byte_buff constructor deduces size with cdat"_test = [] {
+        constexpr auto one          = uint8_t{ 1 };
+        constexpr auto oneone       = static_cast<uint16_t>(one << 8 | one);
+        constexpr auto oneoneoneone = static_cast<uint32_t>(oneone << 16 | oneone);
+
+        constexpr auto buf_ones_a = byte_buff(one, one, one, one);
+        constexpr auto buf_ones_b = byte_buff(oneone, oneone);
+        constexpr auto buf_ones_c = byte_buff(oneoneoneone);
+
+        constexpr auto buf_ones_d = byte_buff(oneone, one, one);
+        constexpr auto buf_ones_e = byte_buff(one, oneone, one);
+        constexpr auto buf_ones_f = byte_buff(one, one, oneone);
+
+        // [a-c] together
+        expect(buf_ones_a == buf_ones_b);
+        expect(buf_ones_a == buf_ones_c);
+        expect(buf_ones_b == buf_ones_c);
+
+        // [d-f] together
+        expect(buf_ones_d == buf_ones_e);
+        expect(buf_ones_d == buf_ones_f);
+        expect(buf_ones_e == buf_ones_f);
+
+        // a with [d-f]
+        expect(buf_ones_a == buf_ones_d);
+        expect(buf_ones_a == buf_ones_e);
+        expect(buf_ones_a == buf_ones_f);
+
+        // b with [d-f]
+        expect(buf_ones_b == buf_ones_d);
+        expect(buf_ones_b == buf_ones_e);
+        expect(buf_ones_b == buf_ones_f);
+
+        // c with [d-f]
+        expect(buf_ones_c == buf_ones_d);
+        expect(buf_ones_c == buf_ones_e);
+        expect(buf_ones_c == buf_ones_f);
+    };
+
+    "span from byte_buff is equal to the original data"_test = [] {
+        constexpr auto one          = uint8_t{ 1 };
+        constexpr auto oneone       = static_cast<uint16_t>(one << 8 | one);
+        constexpr auto oneoneoneone = static_cast<uint32_t>(oneone << 16 | oneone);
+
+        constexpr auto buf_ones_a = byte_buff(one, one, one, one);
+        constexpr auto buf_ones_b = byte_buff(oneone, oneone);
+        constexpr auto buf_ones_c = byte_buff(oneoneoneone);
+
+        constexpr auto buf_ones_d = byte_buff(oneone, one, one);
+        constexpr auto buf_ones_e = byte_buff(one, oneone, one);
+        constexpr auto buf_ones_f = byte_buff(one, one, oneone);
+
+        // [a-c] together
+        expect(std::ranges::equal(buf_ones_a.bytes(), buf_ones_b.bytes()));
+        expect(std::ranges::equal(buf_ones_a.bytes(), buf_ones_c.bytes()));
+        expect(std::ranges::equal(buf_ones_b.bytes(), buf_ones_c.bytes()));
+
+        // [d-f] together
+        expect(std::ranges::equal(buf_ones_d.bytes(), buf_ones_e.bytes()));
+        expect(std::ranges::equal(buf_ones_d.bytes(), buf_ones_f.bytes()));
+        expect(std::ranges::equal(buf_ones_e.bytes(), buf_ones_f.bytes()));
+
+        // a with [d-f]
+        expect(std::ranges::equal(buf_ones_a.bytes(), buf_ones_d.bytes()));
+        expect(std::ranges::equal(buf_ones_a.bytes(), buf_ones_e.bytes()));
+        expect(std::ranges::equal(buf_ones_a.bytes(), buf_ones_f.bytes()));
+
+        // b with [d-f]
+        expect(std::ranges::equal(buf_ones_b.bytes(), buf_ones_d.bytes()));
+        expect(std::ranges::equal(buf_ones_b.bytes(), buf_ones_e.bytes()));
+        expect(std::ranges::equal(buf_ones_b.bytes(), buf_ones_f.bytes()));
+
+        // c with [d-f]
+        expect(std::ranges::equal(buf_ones_c.bytes(), buf_ones_d.bytes()));
+        expect(std::ranges::equal(buf_ones_c.bytes(), buf_ones_e.bytes()));
+        expect(std::ranges::equal(buf_ones_c.bytes(), buf_ones_f.bytes()));
+    };
+}
