d1b827a696db009bd141e300a2c27e3e847ce4c2
Miro Palmu <email@miropalmu.cc>
2024-03-20 13:13:57 +0200

    feat(sstd): impelment integralifyier
    Integralifyier is successor to integral_like

--- include/sstd.hpp
+++ include/sstd.hpp
@@ -40,5 +40,62 @@ struct integral_like {
     }
 };
 
+/// Can be (publicly) inherited to integralifie a type.
+template<std::integral I>
+struct integralifyer {
+    I value;
+    using integral_type = I;
+};
+
+template<typename T>
+concept integralified = std::integral<typename T::integral_type>
+                        and std::derived_from<T, integralifyer<typename T::integral_type>>;
+
+template<typename T>
+concept integral_compatible = std::integral<T> or integralified<T>;
+
+template<typename>
+struct underlying_integral;
+
+template<typename T>
+    requires integralified<std::remove_cvref_t<T>>
+struct underlying_integral<T> {
+    using type = typename std::remove_cvref_t<T>::integral_type;
+};
+
+template<typename T>
+    requires std::integral<std::remove_cvref_t<T>>
+struct underlying_integral<T> {
+    using type = std::remove_cvref_t<T>;
+};
+
+template<typename T>
+using underlying_integral_t = typename underlying_integral<T>::type;
+
+template<typename T>
+    requires integralified<std::remove_cvref_t<T>>
+[[nodiscard]] static auto get_integral_value(T&& x) noexcept -> underlying_integral_t<T> {
+    return std::forward<T>(x).value;
+}
+
+template<typename T>
+    requires std::integral<std::remove_cvref_t<T>>
+[[nodiscard]] static auto get_integral_value(const T x) noexcept -> T {
+    return x;
+}
+
+/// Comparison between integral_compatible types.
+///
+/// Get value of \p x regrdles if it is real integral or integralified one.
+/// Comparison is safe regardless of the underying integral types because
+/// usage of std::cmp_equal.
+template<typename L, typename R>
+    requires integral_compatible<std::remove_cvref_t<L>>
+             and integral_compatible<std::remove_cvref_t<R>>
+[[nodiscard]] constexpr bool operator==(L&& lhs, R&& rhs) {
+    return std::cmp_equal(get_integral_value(std::forward<L>(lhs)),
+                          get_integral_value(std::forward<R>(rhs)));
+}
+
 } // namespace sstd
 } // namespace ger
