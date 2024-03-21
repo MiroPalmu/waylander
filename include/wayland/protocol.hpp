368e6deb13aad6452b0cfee9eeef59968d817a6d
Miro Palmu <email@miropalmu.cc>
2024-03-21 15:11:17 +0200

    feat(connected_client): add method for reserving object id

--- include/wayland/protocol.hpp
+++ include/wayland/protocol.hpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 
 #include "linux_utils/file_descriptor.hpp"
+#include "wayland/protocol_primitives.hpp"
 
 namespace ger {
 namespace wl {
@@ -12,9 +13,15 @@ namespace wl {
 /// Represents one connected client by wrapping the Wayland socket.
 class connected_client {
     linux::fd_type wayland_fd_{};
+    Wuint::integral_type next_new_id_{ 2 };
 
   public:
     [[nodiscard]] connected_client();
+
+    template<typename T = generic_object>
+    [[nodiscard]] auto reserve_object_id() -> Wobject<T> {
+        return { next_new_id_++ };
+    }
 };
 
 } // namespace wl
--- tests/test_wayland_connected_client.cpp
+++ tests/test_wayland_connected_client.cpp
@@ -1,6 +1,10 @@
 #include <boost/ut.hpp> // import boost.ut;
 
+#include <type_traits>
+
 #include "wayland/protocol.hpp"
+#include "wayland/protocol_primitives.hpp"
+#include "wayland/protocols/wayland_protocol.hpp"
 
 int main() {
     using namespace boost::ut;
@@ -13,4 +17,22 @@ int main() {
     wl_tag / "connected_client object can be constructor"_test = [] {
         expect(nothrow([] { auto _ = connected_client(); }));
     };
+
+    wl_tag / "connected_client can reserve object id"_test = [] {
+        auto client    = connected_client{};
+        const auto id1 = client.reserve_object_id<protocols::wl_display>();
+        const auto id2 = client.reserve_object_id<protocols::wl_touch>();
+        const auto id3 = client.reserve_object_id();
+
+        expect(std::same_as<Wobject<protocols::wl_display>, std::remove_cvref_t<decltype(id1)>>);
+        expect(std::same_as<Wobject<protocols::wl_touch>, std::remove_cvref_t<decltype(id2)>>);
+        expect(std::same_as<Wobject<generic_object>, std::remove_cvref_t<decltype(id3)>>);
+        expect(id1 == id1);
+        expect(id1 != id2);
+        expect(id1 != id3);
+        expect(id2 == id2);
+        expect(id2 != id3);
+        expect(id3 == id3);
+    };
+
 }
