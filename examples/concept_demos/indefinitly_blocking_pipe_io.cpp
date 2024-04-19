a7f15cedab9a8528f6f5ea8d3b8af2178eab32ad
Miro Palmu <email@miropalmu.cc>
2024-04-19 12:32:36 +0300

    examples: add indefeinitly_blocking_pipe_io

--- examples/concept_demos/indefinitly_blocking_pipe_io.cpp
+++ examples/concept_demos/indefinitly_blocking_pipe_io.cpp
@@ -1,7 +1,19 @@
+#include <array>
+#include <csignal>
+#include <cstddef>
 #include <print>
+#include <span>
 
+#include "gnu_utils/pipe.hpp"
 
 int main() {
-    std::print("Hello");
-    return 42;
+    std::signal(SIGINT, [](int) { std::println("SIGINT is ignored."); });
+
+    [[maybe_unused]] auto [in, out] = ger::gnu::open_pipe();
+    auto some_data                  = std::array<std::byte, 5>{};
+
+    std::print("Reading... ");
+    // Read should retry after handeled signal.
+    const auto _ = in.read(some_data);
+    std::println("Done!");
 }
