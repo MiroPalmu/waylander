44b5708dfeedef9ac0e9a88fe887efc15cc6e97e
Miro Palmu <email@miropalmu.cc>
2024-05-16 16:48:07 +0300

    feat(sstd): add to_ref_tuple

--- tests/test_sstd_type_utils.cpp
+++ tests/test_sstd_type_utils.cpp
@@ -1,5 +1,6 @@
 #include <boost/ut.hpp> // import boost.ut;
 
+#include <string_view>
 #include <tuple>
 #include <type_traits>
 
@@ -27,5 +28,30 @@ int main() {
         enum foo {};
         expect(sstd::unary_predicate_trait_fold<std::is_enum, foo, foo>);
         expect(sstd::unary_predicate_trait_fold<std::is_floating_point, float, float>);
+    };
+
+    tag("sstd") / "to_ref_tuple is tuple of const references"_test = [] {
+        struct Foo {
+            unsigned a;
+            int b;
+            std::string_view c;
+        };
+
+        const auto foo       = Foo{ 42, 43, "foo object" };
+        const auto ref_tuple = sstd::to_ref_tuple(foo);
+
+        using ref_tuple_type = std::remove_cvref_t<decltype(ref_tuple)>;
+        using expected_ref_tuple_type =
+            std::tuple<const unsigned&, const int&, const std::string_view&>;
+
+        expect(std::is_same_v<ref_tuple_type, expected_ref_tuple_type>);
+
+        expect(std::get<0>(ref_tuple) == foo.a);
+        expect(std::get<1>(ref_tuple) == foo.b);
+        expect(std::get<2>(ref_tuple) == foo.c);
+
+        expect(std::addressof(std::get<0>(ref_tuple)) == std::addressof(foo.a));
+        expect(std::addressof(std::get<1>(ref_tuple)) == std::addressof(foo.b));
+        expect(std::addressof(std::get<2>(ref_tuple)) == std::addressof(foo.c));
     };
 }
