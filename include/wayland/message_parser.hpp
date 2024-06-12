b79aaf6752e2656e9e1736ba1cf4453db541a8c6
Miro Palmu <email@miropalmu.cc>
2024-06-12 16:48:59 +0300

    refactor(wl): separate parsed_message from message_parser

--- include/wayland/message_parser.hpp
+++ include/wayland/message_parser.hpp
@@ -9,19 +9,11 @@
 #include <vector>
 
 #include "byte_vec.hpp"
+#include "wayland/parsed_message.hpp"
 #include "wayland/protocol_primitives.hpp"
 
 namespace ger {
 namespace wl {
-
-/// Parsed Wayland message from raw bytes.
-///
-/// Note that by reading the raw bytes, there is not enough information to determine the type of the object.
-struct parsed_message {
-    wl::Wobject<wl::generic_object> object_id;
-    wl::Wopcode<wl::generic_object> opcode;
-    std::span<const std::byte> arguments;
-};
 
 class message_parser {
     sstd::byte_vec unparsed_messages_;
---
+++ include/wayland/parsed_message.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+/// @file Declares vocabulary type parsed_message.
+
+#include <cstddef>
+#include <span>
+
+#include "wayland/protocol_primitives.hpp"
+
+namespace ger {
+namespace wl {
+
+/// Parsed Wayland message from raw bytes.
+///
+/// Note that by reading the raw bytes, there is not enough information to determine the type of the object.
+struct parsed_message {
+    wl::Wobject<wl::generic_object> object_id;
+    wl::Wopcode<wl::generic_object> opcode;
+    std::span<const std::byte> arguments;
+};
+
+} // namespace wl
+} // namespace ger
